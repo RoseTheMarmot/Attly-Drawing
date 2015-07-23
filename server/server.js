e888e481098819bb77bb86807e04b050816b5149
RoseTheMarmot <rose.beede@gmail.com>
2015-07-23 14:12:16 -0700

    getting ready for hosting

--- server/server.js
+++ server/server.js
@@ -7,9 +7,10 @@ var express = require('express');
 var app = express();
 
 //port
-var port = process.env.PORT || 8000;
-var server = app.listen(port, function(){
-	console.log("\n****************************\n*  listening on port "+port+"  *\n****************************\n");
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server = app.listen(port, ip_address, function(){
+	console.log("\n****************************\n*  listening on port "+port+"  *\n*  IP address: "+ip_address+"   *\n****************************\n");
 });
 
 //client
