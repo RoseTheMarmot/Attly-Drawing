aec2c68d56ed9f1dadd14e9f044dbb2ab41756ce
RoseTheMarmot <rose.beede@gmail.com>
2015-07-29 08:30:51 -0700

    added another port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 //port and ip
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8000;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var server = app.listen(port, ip_address, function(){
 	console.log("\n****************************\n*  listening on port "+port+"  *\n*  IP address: "+ip_address+"   *\n****************************\n");
